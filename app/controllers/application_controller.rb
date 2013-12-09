2d6f452439187580af6fa8d57469b149083a5190
Vincent Cabansag <vcabansag@gmail.com>
2013-12-08 22:49:19 -0600

    Added basic HTTP authentication for staging and production environments.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate
+    if Rails.env.staging? || Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HTTP_USERNAME"] && password == ENV["HTTP_PASSWORD"]
+      end
+    end
+  end
+
 end
