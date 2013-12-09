7df4389190a1a9e99062c707fa2fbd4f89a39e5c
Vincent Cabansag <vcabansag@gmail.com>
2013-12-08 22:16:26 -0600

    Initial commit. Vanilla Rails 4 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cabansag::Application.config.secret_key_base = '15d40e6d9bc4c966e70cf589b01e0e6934f6347e4fad6d0557ae7bebe928282ec40476da00901cd51d767ad8d1d2325415b071679eb63e9172b30f295c5fa57c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cabansag::Application.config.session_store :cookie_store, key: '_cabansag_session'
