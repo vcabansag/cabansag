ff080d229df7ac527b55740c0b0a3cb3ee1e2636
Vincent Cabansag <vcabansag@gmail.com>
2014-02-14 08:24:50 -0600

    Updated helper for text with no logos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,8 @@ module ApplicationHelper
       image_tag('VHS-logo.png', alt: 'VHS', class: 'vhs')
     elsif video_format =='8mm'
       "<span class='eight-mm'>8</span><span class='eight-mm eight-mm-small'>mm</span>"
+    else
+      video_format
     end
   end
 
